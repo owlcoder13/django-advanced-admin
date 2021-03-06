3177b182c807858b7867f3a4a5fe75f1c7e2f31f
owlcoder <owlcoder1@gmail.com>
2021-03-06 20:11:37 +0300

    disable admin for simple users

---
+++ middleware.py
@@ -0,0 +1,24 @@
+from django.http.response import HttpResponseForbidden
+from django.shortcuts import redirect
+
+
+class Middleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if request.path.startswith('/admin') and \
+                (request.user.is_anonymous or not request.user.is_admin()):
+            if request.user.is_authenticated:
+                return HttpResponseForbidden()
+            else:
+                return redirect('/sign-in')
+
+        return None
