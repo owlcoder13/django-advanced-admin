a95aa7bcbdd7f732928b02c6e2dacb66b8f5ff4c
owlcoder <owlcoder1@gmail.com>
2021-07-15 18:18:53 +0300

    dev

--- admin.py
+++ admin.py
@@ -1,15 +1,30 @@
 from .views import AdvancedAdmin
-from django.conf import settings
 import os
+from django.conf import settings
 
 
 def get_settings_root():
     return os.environ.get('DJANGO_SETTINGS_MODULE').split('.')[0]
 
 
+def import_dyn(mod_path):
+    if isinstance(mod_path, str):
+        mod_path = mod_path.split('.')
+
+    return __import__('.'.join(mod_path[0:-1]), fromlist=mod_path)
+
+
 class AdminService(object):
     def __init__(self):
-        self.admin = AdvancedAdmin()
+        admin_class = AdvancedAdmin
+
+        if hasattr(settings, 'ADVANCED_ADMIN_APP_CLASS'):
+            mod_path = settings.ADVANCED_ADMIN_APP_CLASS.split('.')
+            class_name = mod_path[-1]
+
+            admin_class = getattr(import_dyn(mod_path), class_name)
+
+        self.admin = admin_class()
 
     def set_admin(self, admin_app):
         self.admin = admin_app
@@ -17,10 +32,14 @@ class AdminService(object):
     @property
     def routes(self):
 
-        for app in [get_settings_root() + '.admin'] + settings.INSTALLED_APPS:
+        for app in settings.INSTALLED_APPS:
+            print('search app', app)
+            if app == 'advanced_admin':
+                continue
+
             try:
                 mod_path = [app, 'admin']
-                m = __import__('.'.join(mod_path[0:-1]), fromlist=mod_path)
+                m = import_dyn(mod_path)
             except ModuleNotFoundError as ex:
                 pass
 
